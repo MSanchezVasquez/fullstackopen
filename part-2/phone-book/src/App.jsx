9d91113868210ca671d91e4f1e4a854ad6cf7393
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-10-21 11:36:04 -0500

    add usernames to state and validation inputs

---
+++ part-2/phone-book/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedName = newName.trim();
+    if (!trimmedName) return alert("Please enter a valid name");
+    const exists = persons.some(
+      (person) => person.name.toLowerCase() === trimmedName.toLowerCase()
+    );
+    console.log(exists);
+
+    if (exists) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName }));
+    }
+  };
+
+  const handleNameChange = (e) => setNewName(e.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
