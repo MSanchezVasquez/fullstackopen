2de7b5fefc9095db0d62eb433e36a6d65a543232
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-10 18:24:12 -0500

    feat: exercise 3.2 - added info page

--- part-3/phone-book/index.js
+++ part-3/phone-book/index.js
@@ -29,12 +29,16 @@ app.get("/", (request, response) => {
   response.send("<h1>Agenda Telefónica Backend</h1>");
 });
 
-app.get("/info", (request, response) => {
-  response.send("<h1>Phonebook has info for 2 people</h1>");
-});
-
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const currentDate = new Date();
+
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${currentDate}</p>`);
 });
 
 const PORT = 3001;
