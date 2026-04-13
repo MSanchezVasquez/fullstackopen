d24df3742b88654f500008fbc5986c7da7c62cf5
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-04-12 19:04:44 -0500

    feat: exercise 5.3 - allow logged-in users to create new blogs

--- part-5/bloglist-frontend/src/App.jsx
+++ part-5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -34,9 +37,9 @@ const App = () => {
         password,
       });
 
-      setUser(user);
       window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       blogService.setToken(user.token);
+      setUser(user);
       setUsername("");
       setPassword("");
     } catch (e) {
@@ -49,6 +52,25 @@ const App = () => {
 
     setUser(null);
     blogService.setToken(null);
+  };
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault();
+    try {
+      const returnedBlog = await blogService.create({
+        title,
+        author,
+        url,
+      });
+
+      setBlogs(blogs.concat(returnedBlog));
+
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+    } catch (error) {
+      console.error("Error al crear el blog:", error);
+    }
   };
 
   if (user === null) {
@@ -86,6 +108,39 @@ const App = () => {
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
+      <h2>create new</h2>
+      <form onSubmit={handleCreateBlog}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+
+      {/* LISTA DE BLOGS */}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
