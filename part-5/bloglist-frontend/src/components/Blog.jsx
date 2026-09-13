51b7bcbb7c07a108a3bc3441ff0ac917954e90c4
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2026-09-13 18:16:49 -0500

    feat: exercise 5.7 - add toggle functionality to show blog details

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility} style={{ marginLeft: 5 }}>
+          {visible ? "hide" : "view"}
+        </button>
+      </div>
+
+      {visible && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button style={{ marginLeft: 5 }}>like</button>
+          </div>
+          <div>{blog.user ? blog.user.name : "Unknown User"}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
